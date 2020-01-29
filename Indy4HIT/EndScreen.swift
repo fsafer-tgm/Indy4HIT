ed6785c4e35264f24f8a6e1eaa3768d36632f0e7
Patrick <pelias@student.tgm.ac.at>
2020-01-29 10:02:13 +0100

    Added EndScreen.swift and MainMenu.swift and worked on EndScreen.swift.

---
+++ Indy4HIT/EndScreen.swift
@@ -0,0 +1,49 @@
+//
+//  EndScreen.swift
+//  Indy4HIT
+//
+//  Created by Patrick Elias on 29.01.20.
+//  Copyright © 2020 Florian Safer. All rights reserved.
+//
+
+import SpriteKit
+
+class EndScreen: SKScene {
+    init(size: CGSize, didWin: Bool) {
+        super.init(size: size)
+        
+        let msg = didWin ? "You won the Game!" : "You lost the Game!"
+        let text = SKLabelNode(fontNamed: "Arial")
+        let restart = SKLabelNode(fontNamed: "Arial")
+        
+        text.text = msg
+        text.fontSize = 50
+        text.position = CGPoint(x: size.width / 2, y: size.height / 2)
+        
+        restart.text = "Main Menu"
+        restart.fontSize = 40
+        restart.position = CGPoint(x: size.width / 2, y: size.height / 3)
+        restart.name = "mainmenu"
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first
+        let touchLoc = touch?.location(in: self)
+        let touchedNodes = self.nodes(at: touchLoc!)
+        let touchNode = touchedNodes.first
+        switch touchNode?.name {
+        case "mainmenu":
+            SKAction.run {
+                let trans = SKTransition.flipVertical(withDuration: 0.5)
+                let s = MainMenu(size: size)
+                self.view?.presentScene(s, transition: trans)
+            }
+        default:
+            break
+        }
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ Indy4HIT/MainMenu.swift
@@ -0,0 +1,13 @@
+//
+//  MainMenu.swift
+//  Indy4HIT
+//
+//  Created by Patrick Elias on 29.01.20.
+//  Copyright © 2020 Florian Safer. All rights reserved.
+//
+
+import SpriteKit
+
+class MainMenu: SKScene {
+
+}
