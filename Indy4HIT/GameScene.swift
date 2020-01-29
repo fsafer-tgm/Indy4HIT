0eba9b192c6d7a7cb3318e73661510523b1b28e9
Florian Safer <fsafer@student.tgm.ac.at>
2020-01-29 10:21:28 +0100

    Added Image and Background to the Scene

--- Indy4HIT/GameScene.swift
+++ Indy4HIT/GameScene.swift
@@ -9,6 +9,59 @@
 import SpriteKit
 import GameplayKit
 
+struct modeStruct {
+    static let easy = 0
+    static let medium = 1
+    static let hard = 2
+}
+
+
 class GameScene: SKScene {
     
+    let indy = SKSpriteNode(imageNamed: "Indy")
+    var wiese = SKSpriteNode()
+    var gamemode = -1
+    var field = [[SKSpriteNode]]()
+    
+    init(size: CGSize ,selected: Int) {
+        super.init(size: size)
+        if selected == 0{
+            gamemode = modeStruct.easy
+        }else if selected == 1{
+            gamemode = modeStruct.medium
+        }else if selected == 2{
+            gamemode = modeStruct.hard
+        }
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    override func didMove(to view: SKView) {
+        wiese = SKSpriteNode(color: SKColor(displayP3Red: 34/255, green: 139/255, blue: 34/255, alpha: 1), size: CGSize(width: self.size.width-100, height: self.size.height))
+        wiese.position = CGPoint(x: 100+wiese.size.width/2, y: 0+wiese.size.height/2)
+        indy.position = CGPoint(x: 100.0, y: 100.0)
+        indy.size = CGSize(width: 30, height: 30)
+        wiese.addChild(indy)
+        self.addChild(wiese)
+        
+    }
+    
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touches = touches.first
+        let touchLocation = touches?.location(in: self)
+    }
+    
+    func calcField(){
+        if gamemode == modeStruct.easy{
+            
+        }
+    }
+    
+    func random(min: CGFloat, max: CGFloat) -> CGFloat {
+      return CGFloat.random(in: min..<max)
+    }
 }
